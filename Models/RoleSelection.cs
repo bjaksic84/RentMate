e5410f7b8142508045817a238767af7d3cbd07dc
bjaksic84 <bj92996@student.uni-lj.si>
2025-11-04 14:19:41 +0100

    .

--- Models/RoleSelection.cs
+++ Models/RoleSelection.cs
@@ -2,7 +2,7 @@
 {
     public class RoleSelection
     {
-        public string RoleName { get; set; }
+        public string? RoleName { get; set; }
         public bool IsSelected { get; set; }
     }
 }
