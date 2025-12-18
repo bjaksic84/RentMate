212d428dee953052f9414855e9be0335cee78aaa
bjaksic84 <bj92996@student.uni-lj.si>
2025-12-18 11:24:41 +0100

    Dodan MAUI Hybrid App projekt

---
+++ RentMateMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RentMateMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ RentMateMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RentMateMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
